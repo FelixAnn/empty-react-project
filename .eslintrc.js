ddb3b190648d88cd56ed46c28a01246566da1eda
FelixAn <afxin@qq.com>
2021-11-23 19:20:57 +0800

    chore：增加prettier格式化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint", // 使用eslint-config-prettier禁用@typescript-eslint/eslint插件中与prettier冲突的eslint规则
+        "plugin:prettier/recommended" // 启用eslint-plugin-prettier和eslint-config-prettier。这将把prettier的错误显示为ESLint错误。确保这始终是extends阵列中的最后一个配置。
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
